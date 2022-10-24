adb69906253b76bd2d83cfbc834265267cad768b
Ingvar Derešivski <inde999@gmail.com>
2022-10-24 13:18:11 +0300

    ComplexDataModel

---
+++ ContosoUniversity/ContosoUniversity/Models/Instructor.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System;
+
+namespace ContosoUniversity.Models
+{
+    public class Instructor
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Column("FirstMidName")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstName;
+            }
+        }
+
+        public DateTime HireDate { get; set; }
+
+        public ICollection<CourseAssignment> CourseAssignments { get; set; }
+        public OfficeAssignment OfficeAssignment { get; set; }
+    }
+}
